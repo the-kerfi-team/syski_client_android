e2fc97ec0836f523483bfc1d7b665d99eebc6363
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-02-12 20:30:20 +0000

    Added hard-coded testing data & adapter

--- app/src/main/java/uk/co/syski/client/android/SysListActivity.java
+++ app/src/main/java/uk/co/syski/client/android/SysListActivity.java
@@ -6,12 +6,36 @@ import android.widget.ListView;
 
 public class SysListActivity extends AppCompatActivity {
 
+    Integer[] imageArray = {
+            R.drawable.placeholder,
+            R.drawable.placeholder,
+            R.drawable.placeholder
+    };
+
+    String[] hostnames = {
+            "winsys",
+            "unixsys",
+            "testsys"
+    };
+
+    String[] sysNames = {
+            "Test Windows System",
+            "Test UNIX System",
+            "Test System"
+    };
+
     ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sys_list);
+
+        SysListAdapter adapter = new SysListAdapter(this,sysNames,hostnames,imageArray);
+
+        listView = findViewById(R.id.sysList);
+        listView.setAdapter(adapter);
+
     }
 
 
