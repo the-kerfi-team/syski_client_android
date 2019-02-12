2759da0e7c4db512981ba149d7b5d70061971b89
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-02-12 18:22:04 +0000

    Added basic layouts
    Including placeholder for login page and setup for the system list

---
+++ app/src/main/java/uk/co/syski/client/android/MainActivity.java
@@ -0,0 +1,20 @@
+package uk.co.syski.client.android;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openSysList(View view){
+        Intent intent = new Intent(this, SysListActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/uk/co/syski/client/android/SysListActivity.java
@@ -0,0 +1,19 @@
+package uk.co.syski.client.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class SysListActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sys_list);
+    }
+
+
+
+}
