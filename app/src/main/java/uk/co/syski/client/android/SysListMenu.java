1cf0c3433741fea394d809f8fcb5409e3d227949
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-02-14 19:03:21 +0000

    Updated strings

--- app/src/main/java/uk/co/syski/client/android/SysListMenu.java
+++ app/src/main/java/uk/co/syski/client/android/SysListMenu.java
@@ -97,7 +97,7 @@ public class SysListMenu extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        
+
         if (id == R.id.action_settings) {
             Intent settings = new Intent(this, SettingsActivity.class);
             startActivity(settings);
