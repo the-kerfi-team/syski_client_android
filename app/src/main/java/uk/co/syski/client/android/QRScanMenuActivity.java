a997dd465423b386009306da7590124673595937
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-02-17 19:59:43 +0000

    Implemented QR scanning code

--- app/src/main/java/uk/co/syski/client/android/QRScanMenuActivity.java
+++ app/src/main/java/uk/co/syski/client/android/QRScanMenuActivity.java
@@ -1,13 +1,62 @@
 package uk.co.syski.client.android;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class QRScanMenuActivity extends AppCompatActivity {
+
+    Button btnScan;
+    TextView qrTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrscan_menu);
+        initViews();
+    }
+
+    private void initViews(){
+        btnScan = findViewById(R.id.btnScan);
+        qrTxt = findViewById(R.id.txtQR);
+        btnScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                runScanner();
+            }
+        });
+    }
+
+    private void runScanner(){
+        try {
+
+            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+            intent.putExtra("SCAN_MODE", "QR_CODE_MODE"); // "PRODUCT_MODE for bar codes
+
+            startActivityForResult(intent, 0);
+
+        } catch (Exception e) {
+
+            Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
+            Intent marketIntent = new Intent(Intent.ACTION_VIEW,marketUri);
+            startActivity(marketIntent);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+
+            if (resultCode == RESULT_OK) {
+                String contents = data.getStringExtra("SCAN_RESULT");
+                qrTxt.setText(contents);
+            }
+        }
     }
 }
