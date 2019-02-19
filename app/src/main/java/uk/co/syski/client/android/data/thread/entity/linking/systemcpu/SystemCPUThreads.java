ad7d8dc80b4de70a5f42d334a4c271b89a757dab
Dan Lawrence <daniellawrence2468@gmail.com>
2019-02-19 21:05:04 +0000

    Fix build error.

--- app/src/main/java/uk/co/syski/client/android/data/thread/entity/linking/systemcpu/SystemCPUThreads.java
+++ app/src/main/java/uk/co/syski/client/android/data/thread/entity/linking/systemcpu/SystemCPUThreads.java
@@ -4,7 +4,6 @@ import java.util.concurrent.ExecutionException;
 
 import uk.co.syski.client.android.data.entity.linking.SystemCPU;
 import uk.co.syski.client.android.data.thread.entity.linking.systemcpu.statement.InsertAll;
-import uk.co.syski.client.android.data.thread.entity.systemcpu.statement.*;
 
 public class SystemCPUThreads {
     private static final SystemCPUThreads ourInstance = new SystemCPUThreads();
