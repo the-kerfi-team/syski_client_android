7491b9279520c9c9fae0ae0e28a98bf9ff066f3b
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-02-20 22:39:42 +0000

    Implemented displaying CPU data

--- app/src/main/java/uk/co/syski/client/android/cpuActivity.java
+++ app/src/main/java/uk/co/syski/client/android/cpuActivity.java
@@ -2,12 +2,56 @@ package uk.co.syski.client.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
+import uk.co.syski.client.android.data.entity.CPU;
+import uk.co.syski.client.android.data.thread.SyskiCacheThread;
 
 public class cpuActivity extends AppCompatActivity {
+
+    List<CPU> cpuList;
+    TextView model,manufacturer,architecture,clock,core,thread;
+    CPU cpu;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cpu);
+        initViews();
+
+        String sysId = this.getIntent().getStringExtra("SYSTEMID");
+
+        try {
+            cpuList = SyskiCacheThread.getInstance().CPUThreads.GetCPUs(UUID.fromString(sysId));
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        cpu = cpuList.get(0);
+
+        model.setText(cpu.ModelName);
+        manufacturer.setText(cpu.ManufacturerName);
+        architecture.setText(cpu.ArchitectureName);
+        clock.setText(cpu.ClockSpeed);
+        core.setText(cpu.CoreCount);
+        thread.setText(cpu.ThreadCount);
+    }
+
+    private void initViews() {
+        model = findViewById(R.id.cpuModel);
+        manufacturer = findViewById(R.id.cpuMan);
+        architecture = findViewById(R.id.cpuArch);
+        clock = findViewById(R.id.cpuClock);
+        core = findViewById(R.id.cpuCore);
+        thread = findViewById(R.id.cpuThread);
     }
 }
