613846a3698b1873bb309a4bac5edceb08f3a68f
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-02-20 15:59:23 +0000

    Implemented displaying hostname in system overview

--- app/src/main/java/uk/co/syski/client/android/SysOverviewActivity.java
+++ app/src/main/java/uk/co/syski/client/android/SysOverviewActivity.java
@@ -7,6 +7,10 @@ import android.widget.TextView;
 import java.io.Serializable;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
+import uk.co.syski.client.android.data.entity.System;
+import uk.co.syski.client.android.data.thread.SyskiCacheThread;
 
 public class SysOverviewActivity extends AppCompatActivity {
 
@@ -20,10 +24,24 @@ public class SysOverviewActivity extends AppCompatActivity {
         initViews();
 
         String sysId = this.getIntent().getStringExtra("SYSTEMID");
+        system = getSystem(sysId);
 
 
-        textView.setText(sysId);
+        textView.setText(system.HostName);
 
+    }
+
+    private System getSystem(String sysId){
+        List<System> sysList = null;
+        try {
+            sysList = SyskiCacheThread.getInstance().SystemThreads.GetSystems(UUID.fromString(sysId));
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        system = sysList.get(0);
+        return system;
     }
 
     private void initViews(){
