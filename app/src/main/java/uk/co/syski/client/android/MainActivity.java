d0ca941de12189b5c4961327da4095a6570444f1
JLScott1999 <git@jlscott.co.uk>
2019-02-20 17:26:56 +0000

    Get UUID from response

--- app/src/main/java/uk/co/syski/client/android/MainActivity.java
+++ app/src/main/java/uk/co/syski/client/android/MainActivity.java
@@ -149,7 +149,7 @@ public class MainActivity extends AppCompatActivity {
         protected void RequestSuccessful(JSONObject response) {
             User user = new User();
             try {
-                user.Id = UUID.fromString("38400000-8cf0-11bd-b23e-10b96e4ef00d");
+                user.Id = UUID.fromString(response.getString("uuid"));
                 user.Email = response.getString("email");
                 user.AccessToken = response.getString("token");
                 user.RefreshToken = response.getString("refreshToken");
