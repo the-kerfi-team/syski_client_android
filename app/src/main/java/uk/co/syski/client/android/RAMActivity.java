9c1fd712bca6e4bad25f1061d0097036b6225e16
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-02-24 19:52:29 +0000

    Setup RAM views

--- app/src/main/java/uk/co/syski/client/android/RAMActivity.java
+++ app/src/main/java/uk/co/syski/client/android/RAMActivity.java
@@ -2,12 +2,27 @@ package uk.co.syski.client.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class RAMActivity extends AppCompatActivity {
+
+    TextView model,manufacturer,type,size;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ram);
+
+        initViews();
     }
+
+    private void initViews(){
+        model = findViewById(R.id.txtRamModel);
+        manufacturer = findViewById(R.id.txtRamManufacturer);
+        type = findViewById(R.id.txtRamType);
+        size = findViewById(R.id.txtRamSize);
+    }
+
 }
