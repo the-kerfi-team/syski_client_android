3ee43ca1931fe91f7114e79918dbb8cdf878e660
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-02-25 17:14:51 +0000

    Implemented displaying storage information

--- app/src/main/java/uk/co/syski/client/android/StorageActivity.java
+++ app/src/main/java/uk/co/syski/client/android/StorageActivity.java
@@ -6,14 +6,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
+import uk.co.syski.client.android.data.entity.RAMEntity;
+import uk.co.syski.client.android.data.entity.StorageEntity;
+import uk.co.syski.client.android.data.thread.SyskiCacheThread;
+
 public class StorageActivity extends AppCompatActivity {
 
     TextView manufacturer,size,type,name;
+    List<StorageEntity> storageList;
+    StorageEntity storage;
     SharedPreferences prefs;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_storage);
+
+        initViews();
+        getStorage();
+
+        manufacturer.setText(storage.ManufacturerName);
+        size.setText(""+storage.MemoryBytes);
+        type.setText(storage.MemoryTypeName);
+        name.setText(storage.ModelName);
+
+    }
+
     private void initViews() {
         prefs = this.getSharedPreferences(
                 getString(R.string.preference_sysID_key), Context.MODE_PRIVATE);
@@ -21,5 +46,20 @@ public class StorageActivity extends AppCompatActivity {
         size = findViewById(R.id.txtStorageSize);
         type = findViewById(R.id.txtStorageType);
         name = findViewById(R.id.txtStoreModel);
+    }
+
+    private void getStorage() {
+        String sysId = prefs.getString(getString(R.string.preference_sysID_key), null);
+        try {
+            storageList = SyskiCacheThread.getInstance().StorageThreads.GetStorages(UUID.fromString(sysId));
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        if(storageList.size() > 0){
+            storage = storageList.get(0);
+        }
     }
 }
