cbeba3237df0d5ffcf8aaea50d9589994924d317
Dylan Currey <9024684+dscurrey@users.noreply.github.com>
2019-02-25 17:13:51 +0000

    Added views to storage activity

--- app/src/main/java/uk/co/syski/client/android/StorageActivity.java
+++ app/src/main/java/uk/co/syski/client/android/StorageActivity.java
@@ -1,13 +1,25 @@
 package uk.co.syski.client.android;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StorageActivity extends AppCompatActivity {
 
+    TextView manufacturer,size,type,name;
+    SharedPreferences prefs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_storage);
+    private void initViews() {
+        prefs = this.getSharedPreferences(
+                getString(R.string.preference_sysID_key), Context.MODE_PRIVATE);
+        manufacturer = findViewById(R.id.txtStorageManufacturer);
+        size = findViewById(R.id.txtStorageSize);
+        type = findViewById(R.id.txtStorageType);
+        name = findViewById(R.id.txtStoreModel);
     }
 }
