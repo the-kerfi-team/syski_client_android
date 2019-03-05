e537f01259e8728fd13fc97637996d92c23a01e7
JLScott1999 <git@jlscott.co.uk>
2019-03-05 11:06:17 +0000

    Fix wrong query parameter and Token Refresh URL

--- app/src/main/java/uk/co/syski/client/android/data/dao/MotherboardDao.java
+++ app/src/main/java/uk/co/syski/client/android/data/dao/MotherboardDao.java
@@ -13,7 +13,7 @@ import uk.co.syski.client.android.data.entity.MotherboardEntity;
 @Dao
 public interface MotherboardDao {
 
-    @Query("SELECT * FROM MotherboardEntity WHERE Id IN :Id")
+    @Query("SELECT * FROM MotherboardEntity WHERE Id == :Id")
     MotherboardEntity GetMotherboard(UUID Id);
 
     @Query("SELECT * FROM MotherboardEntity WHERE Id IN (:Ids)")
