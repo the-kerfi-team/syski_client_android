5cbc7715437c9e8b6ce7f597812c20cd7885c319
Dan Lawrence <daniellawrence2468@gmail.com>
2019-03-12 21:59:08 +0000

    Add graph activities.

---
+++ app/src/main/java/uk/co/syski/client/android/view/graph/VariableCPUGraph.java
@@ -0,0 +1,21 @@
+package uk.co.syski.client.android.view.graph;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jjoe64.graphview.GraphView;
+
+import uk.co.syski.client.android.R;
+
+public class VariableCPUGraph extends AppCompatActivity {
+
+    GraphView graph;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_variable_cpugraph);
+
+        graph = findViewById(R.id.graph);
+    }
+}
