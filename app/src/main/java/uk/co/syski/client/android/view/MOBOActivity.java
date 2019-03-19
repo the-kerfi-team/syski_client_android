f1ecba9035fc036178528ea53ee66ebfc9b5f0dc
Dan Lawrence <daniellawrence2468@gmail.com>
2019-03-19 23:21:51 +0000

    Fully implement Motherboard activity.

--- app/src/main/java/uk/co/syski/client/android/view/MOBOActivity.java
+++ app/src/main/java/uk/co/syski/client/android/view/MOBOActivity.java
@@ -1,10 +1,22 @@
 package uk.co.syski.client.android.view;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import uk.co.syski.client.android.R;
+import uk.co.syski.client.android.data.entity.MotherboardEntity;
+import uk.co.syski.client.android.model.HeadedValueModel;
+import uk.co.syski.client.android.view.adapters.ComponentDataListAdapter;
+import uk.co.syski.client.android.view.fragment.DoubleHeadedValueFragment;
+import uk.co.syski.client.android.viewmodel.MotherboardViewModel;
 
 public class MOBOActivity extends AppCompatActivity {
 
@@ -13,7 +25,42 @@ public class MOBOActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cpu);
+        setContentView(R.layout.activity_motherboard);
+
+        MotherboardViewModel viewModel = ViewModelProviders.of(this).get(MotherboardViewModel.class);
+        viewModel.get().observe(this, new Observer<List<MotherboardEntity>>() {
+            @Override
+            public void onChanged(@Nullable List<MotherboardEntity> motherboardEntities) {
+                if (motherboardEntities.size() > 0) {
+                    updateStaticUI(motherboardEntities.get(0));
+                }
+            }
+        });
+    }
+
+    private void updateStaticUI(MotherboardEntity motherboardEntity) {
+        ArrayList<HeadedValueModel> motherboardData = new ArrayList<>();
+
+        motherboardData.add(
+            new HeadedValueModel(
+                R.drawable.ic_version,
+                "Version",
+                motherboardEntity.Version
+            )
+        );
+
+        DoubleHeadedValueFragment topFragment = DoubleHeadedValueFragment.newInstance(
+            R.drawable.ic_gpu,
+            "Model",
+            motherboardEntity.ModelName,
+            "Manufacturer",
+            motherboardEntity.ManufacturerName
+        );
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.topFragment, topFragment).commit();
+
+        ListView dataList = findViewById(R.id.listView);
+        dataList.setAdapter(new ComponentDataListAdapter(this, motherboardData));
     }
 
 }
